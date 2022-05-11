b21446441bd15136cad89ab5f562b342d7b77704
Marcela <mrssolaris.dev@gmail.com>
2022-05-11 15:59:40 -0300

    [Config] Configuração inicial

---
+++ src/components/Game.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
