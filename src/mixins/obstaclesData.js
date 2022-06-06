a7408e9634e42c7692ac202c6eb13fb484e6b9a8
Marcela <mrssolaris.dev@gmail.com>
2022-06-06 19:30:17 -0300

    Transfere obstaculos para console

--- src/mixins/obstaclesData.js
+++ src/mixins/obstaclesData.js
@@ -3,13 +3,13 @@ export const obstaclesData = {
         return {
             obstacles: {
                 birdy: {
-                    image: null,
-                    posX: 0,
-                    posY: 0,
-                    width: 75,
-                    height: 75,
-                    hitDino: false,
-                    birdy: true
+                  image: null,
+                  posX: 0,
+                  posY: 0,
+                  width: 75,
+                  height: 75,
+                  hitDino: false,
+                  birdy: true
                 },
                 rock: {
                     image: null,
@@ -29,16 +29,7 @@ export const obstaclesData = {
                     hitDino: false,
                     star: true
                 },
-            },
+            }
         }
     },
-    computed: {
-        // Fiz essa computed pra não precisar escrever tanto e deixar o código menos poluido. Mas retorna o mesmo dado.
-        dinoDimensions() {
-            return this.dinoCharacterData.dimensions
-        }
-    },
-    created() {
-
-    }
 }
